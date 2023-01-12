f724cb68a8b524d1d2069be350b74ef46fa93680
nballar <naballar@gmail.com>
2023-01-12 21:57:36 +0000

    Saved progress on Average 2023-01-12 21:57:36 +0000

--- Average/src/main/java/Average.java
+++ Average/src/main/java/Average.java
@@ -18,7 +18,7 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
-        int sum = 0;
+        double sum = 0;
         for(int i = 0; i < nums.length; i++){
             sum = sum + nums[i];
         }

0d5c8ac0a7032ae897cd2f618de162ae2afe89fb
nballar <naballar@gmail.com>
2023-01-13 21:51:08 +0000

    Saved progress on MaxMinusMin 2023-01-13 21:51:08 +0000

--- MaxMinusMin/src/main/java/MaxMinusMin.java
+++ MaxMinusMin/src/main/java/MaxMinusMin.java
@@ -1,5 +1,6 @@
 
 import java.util.List;
+import java.util.Collections;
 
 public class MaxMinusMin {
     /**
@@ -10,6 +11,8 @@ public class MaxMinusMin {
      * @return the max value of nums minus the min value of nums.
      */
     public int difference(List<Integer> nums){
-        return 0;
+        int max = Collections.max(nums);
+        int min = Collections.min(nums);
+        return max - min;
     }
 }
